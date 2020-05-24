dce420ca7c1297bfec4e214b7ae058ae91ab60fa
Alex M <a.mark159@gmail.com>
2020-05-24 03:10:11 -0400

    * Fixed bug with null terminator moved/discarded in C-style string

--- range.cpp
+++ range.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <memory>
 #include <optional>
+#include <string.h>
 //#include <istream>
 /*
 struct Rope{
@@ -63,18 +64,25 @@ bool is_num(char c){
 }
 
 
-bool in_range(std::string check, std::string low, std::string hi){
+bool in_range(char* check, int start, int end, std::string low, std::string hi){
     
-    if(check.length() < low.length() || hi.length() < check.length() )
+    unsigned int len = end - start;
+    if( len < low.length() || hi.length() < len )
         return false;
     
     // if check is between the length of low and hi, then it must also have 
     // a value in between as well.
-    if(low.length() < check.length() && check.length() < hi.length() )
+    if(low.length() < len && len < hi.length() )
         return true;
 
     // this will only run if check is equal in length to either low or hi
-    if(low.length() == check.length()){
+    if(low.length() == len){
+        int ret = strncmp(low.c_str(), check, len);
+        if(ret < 0)
+            return true;
+        else
+            return false;
+        /*        
         for(int i = 0; i < static_cast<int>(check.length()); i++){
             if(low[i] != check[i]) {
                 if( check[i] < low[i]  ) {
@@ -84,7 +92,14 @@ bool in_range(std::string check, std::string low, std::string hi){
                 }
             }
         }
-    } else if(hi.length() == check.length()) {
+        */
+    } else if(hi.length() == len) {
+        int ret = strncmp(check, hi.c_str(), len);
+        if(ret < 0)
+            return true;
+        else
+            return false;
+        /*
         for(int i = 0; i < static_cast<int>(check.length()); i++){
             if(hi[i] != check[i]) {
                 if( hi[i] < check[i]  ) {
@@ -94,8 +109,10 @@ bool in_range(std::string check, std::string low, std::string hi){
                 }
             }
         }
+        */
     }
     return true;
+    
 }
 
 
@@ -113,15 +130,16 @@ int main(int argc, char* argv[]){
     int dist;
     int prev;
     int curr;
-    bool print_this_line;
-    std::string lo;
-    std::string hi;
+    bool print_this_line = false;
+    std::string _lo;
+    std::string _hi;
+    
     std::string file;
     std::ifstream file_stream;
     constexpr size_t buffersize = 4026 * 32;
     
-    lo.assign(argv[1]);
-    hi.assign(argv[2]);
+    _lo.assign(argv[1]);
+    _hi.assign(argv[2]);
     file.assign(argv[3]);
     //std::cout << "file: " << file << std::endl;
     file_stream.open(file);
@@ -131,14 +149,19 @@ int main(int argc, char* argv[]){
         std::exit(1);
     }
     
-    std::vector<char> text(buffersize);
+    std::vector<char> _text(buffersize);
+    std::vector<char> _temp(buffersize);
+    char* text = &_text[0];
+    char* temp = &_temp[0];
+    //const char* lo = _lo.c_str();
+    //const char* hi = _hi.c_str();
     prev = -1;
     dist = -1;
     curr = 0;
     
-    file_stream.read(text.data(), buffersize);
+    file_stream.read(text, buffersize);
         
-    if( file_stream .fail() && !file_stream.eof() ) {
+    if( file_stream.fail() && !file_stream.eof() ) {
         std::cout << "exiting due to file stream error" << std::endl;
         
         std::exit(1);
@@ -160,22 +183,9 @@ int main(int argc, char* argv[]){
                 // we're at the end of a number
                 // print everything from dist to prev
                 // mark this line as needing to be printed
-                std::string match(text.begin() + dist, text.begin() + prev + 1);
-                /*                
-                if(dist == -1)
-                    std::cout << "dist: -1" << std::endl;
-                else
-                    std::cout << "dist: " << text[dist] << std::endl;
-                std::cout << "prev: " << text[prev] << std::endl;
-                std::cout << "curr: " << text[curr] << std::endl;
-                */
-                //std::cout << "match: " << match.c_str() << std::endl;
-                //std::cout << "low: " << lo << std::endl;
-                //std::cout << "high: " << hi << std::endl;
                 
-                if( in_range(match, lo, hi) ) {
-                    //std::cout << "match: " << match.c_str() << std::endl;
-                    //std::cout << "in range" << std::endl;
+                if( in_range(text, dist, prev+1, _lo, _hi) ) {
+                    
                     print_this_line = true;
                 }
                 prev = curr;
@@ -183,8 +193,8 @@ int main(int argc, char* argv[]){
             }
             if( text[curr] == '\n' && print_this_line) {
                 // end of the line. Print if we need to...
-                std::string line(&text[start_of_line], &text[curr]);
-                std::cout << "line: " << line << std::endl;
+                
+                printf("%.*s", (curr - start_of_line)+1, &text[start_of_line]);
                 print_this_line = false;
             }
             if( text[prev] == '\n' ) {
@@ -196,18 +206,20 @@ int main(int argc, char* argv[]){
         file_stream.clear();
        
         if(text[curr] != '\n') {
-            // our buffer ends in the middle of a line, so we have to save that 
-            std::vector<char> temp(text.begin() + start_of_line, text.begin() + curr);
-            std::move(temp.begin(), temp.end(), text.data());
-            file_stream.read(text.data() + temp.size(), buffersize - temp.size());
-            curr = curr - start_of_line;
+            // our buffer ends in the middle of a line, so we have to save that
+            int saved_len = (curr - start_of_line);
+
+            strncpy(temp, &text[start_of_line], saved_len);
+            strncpy(text, temp, saved_len);            
+            file_stream.read( &text[saved_len], buffersize - saved_len - 1);
+
+            curr = saved_len;
             if( dist != -1 )
                 dist = dist - start_of_line;
             prev = curr - 1;
             start_of_line = 0;
-            print_this_line = false;
         } else {
-            file_stream.read(text.data(), buffersize);
+            file_stream.read(text, buffersize);
             curr = 0;
             dist = -1;
             prev = curr - 1;
@@ -216,42 +228,8 @@ int main(int argc, char* argv[]){
         }
         
     }
-    
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
